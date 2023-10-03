66085c1ada8bcfc681042794b18300a6e9ac19f6
Marco Zellini <zellinimarco@gmail.com>
2023-10-03 12:10:08 +0200

    Add SCSS Variables

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <header id="app_header">
+
+    </header>
+    <!-- /#app_header -->
+
+    <main id="app_main">
+
+    </main>
+    <!-- /#app_main -->
+
+    <footer id="app_footer">
+
+    </footer>
+    <!-- /#app_footer -->
+</template>
+
+<style></style>
