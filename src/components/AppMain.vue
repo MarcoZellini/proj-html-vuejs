c21f224d69c2030fd586bc80af190725542d0273
Marco Zellini <zellinimarco@gmail.com>
2023-10-04 10:44:02 +0200

    Fix arrows

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -52,8 +52,18 @@ export default {
             <div class="slider" @mouseover="this.stopSlider" @mouseleave="this.startSlider">
                 <img class="img-fluid" :src="getImageUrl(this.slides[this.indexActiveSlide])" alt="">
                 <div class="commands container_90">
-                    <div class="prev" @click="prevImage">&larr;</div>
-                    <div class="next" @click="nextImage">&rarr;</div>
+                    <div class="prev" @click="prevImage">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" style="height:20px;">
+                            <path
+                                d="M4 15a1 1 0 001 1h19.586l-4.292 4.292a1 1 0 001.414 1.414l6-6a.99.99 0 00.292-.702V15c0-.13-.026-.26-.078-.382a.99.99 0 00-.216-.324l-6-6a1 1 0 00-1.414 1.414L24.586 14H5a1 1 0 00-1 1z" />
+                        </svg>
+                    </div>
+                    <div class="next" @click="nextImage">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" style="height:20px">
+                            <path
+                                d="M4 15a1 1 0 001 1h19.586l-4.292 4.292a1 1 0 001.414 1.414l6-6a.99.99 0 00.292-.702V15c0-.13-.026-.26-.078-.382a.99.99 0 00-.216-.324l-6-6a1 1 0 00-1.414 1.414L24.586 14H5a1 1 0 00-1 1z" />
+                        </svg>
+                    </div>
                 </div>
                 <div class="container">
                     <div class="row">
@@ -132,8 +142,23 @@ export default {
             <div class="container">
                 <div class="row">
 
+                    <div class="slider">
+                        <img class="img-fluid" src="../assets/img/h1-img-01.jpg" alt="">
+                        <div class="commands container_90">
+                            <div class="prev" @click="prevImage"><svg xmlns="http://www.w3.org/2000/svg" width="32"
+                                    height="32" style="height:20px">
+                                    <path
+                                        d="M4 15a1 1 0 001 1h19.586l-4.292 4.292a1 1 0 001.414 1.414l6-6a.99.99 0 00.292-.702V15c0-.13-.026-.26-.078-.382a.99.99 0 00-.216-.324l-6-6a1 1 0 00-1.414 1.414L24.586 14H5a1 1 0 00-1 1z" />
+                                </svg></div>
+                            <div class="next" @click="nextImage"><svg xmlns="http://www.w3.org/2000/svg" width="32"
+                                    height="32" style="height:20px">
+                                    <path
+                                        d="M4 15a1 1 0 001 1h19.586l-4.292 4.292a1 1 0 001.414 1.414l6-6a.99.99 0 00.292-.702V15c0-.13-.026-.26-.078-.382a.99.99 0 00-.216-.324l-6-6a1 1 0 00-1.414 1.414L24.586 14H5a1 1 0 00-1 1z" />
+                                </svg></div>
+                        </div>
+                    </div>
 
-                    <img class="img-fluid" src="../assets/img/h1-img-01.jpg" alt="">
+
                     <div class="founder_info">
                         <h2 class="name">jason bickford</h2>
                         <small class="role">Founder and Executive Director</small>
